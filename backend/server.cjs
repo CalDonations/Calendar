fb8021814af0ba728990dc51054611b61c811768
Eddie <vvvdwbvvv@gmail.com>
2024-10-01 14:49:37 +0800

    hotfix/#2

--- backend/server.cjs
+++ backend/server.cjs
@@ -5,6 +5,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { connectToDatabase } = require("./db");  
 
 const app = express();
 const PORT = process.env.PORT || 3000;
