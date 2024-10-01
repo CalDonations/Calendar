4ba01742414ae8dcd3c22b934eee3011468d148f
Eddie Tsai <vvvdwbvvv@gmail.com>
2024-10-01 10:51:17 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/你的Github repository名字/",
+    base: "/CalDonations/",
     plugins: [react()],
     resolve: {
         alias: {
