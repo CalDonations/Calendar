c0623850467808353335a7da1863abd8d8056c18
Eddie Tsai <vvvdwbvvv@gmail.com>
2024-10-02 22:37:55 +0800

    Update server.cjs

--- backend/server.cjs
+++ backend/server.cjs
@@ -5,7 +5,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { connectToDatabase } = require("./db");  
 
 const app = express();
 const PORT = process.env.PORT || 3000;
