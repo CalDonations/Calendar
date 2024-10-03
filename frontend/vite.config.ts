d9e60df7f076ddf6bb9280909c6f05c3c9c39ca8
Eddie <vvvdwbvvv@gmail.com>
2024-10-03 18:22:10 +0800

    dev/#3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,17 @@
 import path from "path"
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/Deploying-vite/",
+    base: "/Calendar/",
     plugins: [react()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    build: {
+        sourcemap: true,  // Enable sourcemap in build
+    }
 })
